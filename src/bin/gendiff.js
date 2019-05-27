205d17801fa31fb833ec068e84bb18c8c2a42972
Bykov Slava <avel2k6@gmail.com>
2019-05-27 13:18:58 +0300

    init new hexlet project

---
+++ src/bin/gendiff.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+import main from '..';
+import commander from 'commander';
+
+const program = new commander.Command();
+
+program
+  .version('0.0.1')
+  .arguments('<firstConfig> <secondConfig>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .action((firstConfig, secondConfig, options) => {
+    console.log(firstConfig);
+    console.log(secondConfig);
+    console.log(options);
+    main();
+  });
+
+program.parse(process.argv);
